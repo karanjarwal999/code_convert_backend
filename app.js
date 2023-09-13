1d8fe57ecb46b2b697c574d136b9ce9e96fd0fe4
karanjarwal999 <karanjarwal999@gmail.com>
2023-09-13 23:06:02 +0530

    ok

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Define routes
-console.log(1)
+
 app.get('/',(req,res)=>{
     res.send('Code converter api ')
 })
@@ -34,7 +34,7 @@ app.post('/code-converter', CodeConverter);
 app.post('/debug',CodeDebuger);
 
 app.post('/quality-check', CodeQualityChecker);
-console.log(2)
+
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -43,7 +43,7 @@ passport.use(new GitHubStrategy({
   // Store user data in session or database
   return done(null, profile);
 }));
-console.log(3)
+
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -51,14 +51,29 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
-console.log(4)
-app.get('/auth/github', passport.authenticate('github'));
+
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
   (req, res) => {
     res.redirect('/'); // Redirect to the dashboard or code conversion page
   }
 );
+
+app.get('/repos', (req, res) => {
+  const accessToken = req.user.accessToken;
+  axios.get('https://api.github.com/user/repos', {
+      headers: {
+          Authorization: `Bearer ${accessToken}`
+      }
+  })
+  .then(response => {
+      res.json(response.data);
+  })
+  .catch(error => {
+      res.status(500).send('Error fetching repositories');
+  });
+  
+});
 
 // Start the server
 app.listen(8080, () => {
